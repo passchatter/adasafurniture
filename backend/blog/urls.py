3105b2555a2728ca82363ae856a4ce2f942f8fab
pastika <pastikaerror2625@gmail.com>
2024-05-05 01:57:36 +0800

    ubah name blog

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GetBlog, name="getblog"),
+    path('list/', views.GetBlog, name="getblog"),
     path('detail/<int:pk>/', views.getDetailBlog, name="detailblog"),
     path('contact/', views.contactForm, name="contactform"),
     path('sendemail/', views.SendEmail.as_view(), name='sendemail')
